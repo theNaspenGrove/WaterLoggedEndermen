53c57312d5920114d6a8dbaee3edd39166bdb2a3
mov <mov51@mov51.net>
2023-07-17 02:33:10 -0400

    Initial commit

---
+++ src/main/java/net/mov51/waterloggedendermen/WaterLoggedEndermen.java
@@ -0,0 +1,17 @@
+package net.mov51.waterloggedendermen;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WaterLoggedEndermen extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
